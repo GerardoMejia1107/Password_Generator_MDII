27ec8062698a57d3a11c1f8b31d25a69f65d35a2
GerardoMejia1107 <00104923@uca.edu.sv>
2024-11-11 18:52:55 -0600

    💥 Datos de analisis de seguridad
    Funciones que calculan datos relacionados al analisis en base a los parametros que el usuario escoge para la generacion de su contraseña

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,38 +3,33 @@ import './App.css';
 
 function App() {
     const [length, setLength] = useState(8);
-    const [pool, setPool] = useState('');
     const [includeUppercase, setIncludeUppercase] = useState(false);
+    const [includeLowerCase, setIncludeLowercase] = useState(false);
     const [includeNumbers, setIncludeNumbers] = useState(false);
     const [includeSymbols, setIncludeSymbols] = useState(false);
     const [generatedPassword, setGeneratedPassword] = useState('');
-    const [combinations, setCombinations] = useState('---');
-    const [weakProbability, setWeakProbability] = useState('---');
-    const [secureProbability, setSecureProbability] = useState('---');
+    const [combinations, setCombinations] = useState('');
+    const [secureProbability, setSecureProbability] = useState('');
+    const [entropy, setEntropy] = useState('');
+    const [security, setSecurity] = useState('');
 
     const generatePassword = () => {
         const lowercase = 'abcdefghijklmnopqrstuvwxyz';
         const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
         const numbers = '0123456789';
-        const symbols = '!@#$%^&*()_-+=<>?';
+        const symbols = '!@#$%^&*()_+-=[]{}|;:\'",.<>?/`~\\©®£¥€\n';
 
-        let characters = lowercase;
+        let characters = ''; // Inicia como cadena vacía
 
-        if (includeUppercase) {
-            characters += uppercase;
-        }
-        if (includeNumbers) {
-            characters += numbers;
-        }
-        if (includeSymbols) {
-            characters += symbols;
-        }
+        if (includeLowerCase) characters += lowercase;
+        if (includeUppercase) characters += uppercase;
+        if (includeNumbers) characters += numbers;
+        if (includeSymbols) characters += symbols;
 
-        if (!characters) {
-            characters = lowercase;
+        if (characters.length === 0) {
+            alert('Selecciona al menos una opción de caracteres');
+            return;
         }
-
-        setPool(characters);
 
         let password = '';
         for (let i = 0; i < length; i++) {
@@ -44,20 +39,28 @@ function App() {
 
         setGeneratedPassword(password);
 
-        // Calcular combinaciones después de generar la contraseña
-        const result = getPermutations(characters.length, length);
-        setCombinations(formatNumberWithCommas(result));
+        // Calcula combinaciones y probabilidad
+        const totalCombinations = Math.pow(characters.length, length);
+        setCombinations(totalCombinations.toLocaleString());
+        setSecureProbability((1 / totalCombinations).toExponential(2));
+
+        // Calcula entropía y clasifica la seguridad
+        const entropyValue = calcEntropy(length, characters.length);
+        setEntropy(entropyValue.toFixed(2));
+        setSecurity(categorizeSecurity(entropyValue));
     };
 
     const resetInputs = () => {
         setLength(8);
         setIncludeUppercase(false);
+        setIncludeLowercase(false);
         setIncludeNumbers(false);
         setIncludeSymbols(false);
         setGeneratedPassword('');
-        setCombinations('---');
-        setWeakProbability('---');
-        setSecureProbability('---');
+        setCombinations('');
+        setSecureProbability('');
+        setEntropy('');
+        setSecurity('');
     };
 
     const copyToClipboard = () => {
@@ -69,72 +72,97 @@ function App() {
         }
     };
 
-    const factorial = (number) => {
-        if (number === 0 || number === 1) {
-            return 1;
+    const calcEntropy = (passwordLength, totalPool) => {
+        return passwordLength * Math.log2(totalPool);
+    };
+
+    const categorizeSecurity = (entropyValue) => {
+        if (entropyValue < 28) {
+            return "Muy Débil";
+        } else if (entropyValue >= 28 && entropyValue <= 35) {
+            return "Débil";
+        } else if (entropyValue >= 36 && entropyValue <= 59) {
+            return "Razonable";
+        } else if (entropyValue >= 60 && entropyValue <= 127) {
+            return "Fuerte";
+        } else {
+            return "Muy Fuerte";
         }
-        return number * factorial(number - 1);
-    };
-
-    const getPermutations = (n, k) => {
-        return factorial(n) / factorial(n - k);
-    };
-
-    const formatNumberWithCommas = (number) => {
-        return number.toLocaleString();
     };
 
     return (
         <main className="generator-program-container">
-            <div className="container">
-                <div className="config-section">
-                    <h1>Generador de Contraseñas</h1>
-                    <label htmlFor="length">Longitud:</label>
-                    <input
-                        type="number"
-                        id="length"
-                        min="4"
-                        max="20"
-                        value={length}
-                        onChange={(e) => setLength(Number(e.target.value))}
-                    />
+            <section className="config-section">
+                <h1>Generador de Contraseñas</h1>
+                <label htmlFor="length">Longitud:</label>
+                <input
+                    type="number"
+                    id="length"
+                    min="4"
+                    max="20"
+                    value={length}
+                    onChange={(e) => setLength(Number(e.target.value))}
+                />
 
-                    <div className="options">
-                        <label><input type="checkbox" checked={includeUppercase} onChange={() => setIncludeUppercase(!includeUppercase)} /> Mayúsculas</label>
-                        <label><input type="checkbox" checked={includeNumbers} onChange={() => setIncludeNumbers(!includeNumbers)} /> Números</label>
-                        <label><input type="checkbox" checked={includeSymbols} onChange={() => setIncludeSymbols(!includeSymbols)} /> Símbolos</label>
-                    </div>
-
-                    <div className="button-group">
-                        <button onClick={generatePassword}>Generar</button>
-                        <button onClick={resetInputs}>Reiniciar</button>
-                    </div>
+                <div className="options">
+                    <label>
+                        <input type="checkbox" checked={includeLowerCase}
+                               onChange={() => setIncludeLowercase(!includeLowerCase)}/>
+                        Minúsculas
+                    </label>
+                    <label>
+                        <input type="checkbox" checked={includeUppercase}
+                               onChange={() => setIncludeUppercase(!includeUppercase)}/>
+                        Mayúsculas
+                    </label>
+                    <label>
+                        <input type="checkbox" checked={includeNumbers}
+                               onChange={() => setIncludeNumbers(!includeNumbers)}/>
+                        Números
+                    </label>
+                    <label>
+                        <input type="checkbox" checked={includeSymbols}
+                               onChange={() => setIncludeSymbols(!includeSymbols)}/>
+                        Símbolos
+                    </label>
                 </div>
 
-                <div className="result-section">
-                    <h2>Contraseña Generada:</h2>
-                    <input type="text" value={generatedPassword} readOnly />
-                    <button onClick={copyToClipboard}>Copiar</button>
-
-                    <div className="security-analysis">
-                        <h3>Análisis de Seguridad</h3>
-                        <div className="result-cards">
-                            <div className="result-card">
-                                <i className="fas fa-lock"></i>
-                                <p>Combinaciones: <span>{combinations}</span></p>
-                            </div>
-                            <div className="result-card weak">
-                                <i className="fas fa-exclamation-triangle"></i>
-                                <p>Débil: <span>{weakProbability}</span></p>
-                            </div>
-                            <div className="result-card secure">
-                                <i className="fas fa-shield-alt"></i>
-                                <p>Segura: <span>{secureProbability}</span></p>
-                            </div>
-                        </div>
-                    </div>
+                <div className="button-group">
+                    <button onClick={generatePassword}>Generar</button>
+                    <button onClick={resetInputs}>Reiniciar</button>
                 </div>
-            </div>
+            </section>
+
+            <section className="result-section">
+                <h2>Contraseña Generada:</h2>
+                <input type="text" value={generatedPassword} readOnly/>
+                <button onClick={copyToClipboard}>Copiar</button>
+            </section>
+
+            <section className="security-analysis-section">
+                <h3>Análisis de Seguridad</h3>
+
+                <table className="security-table">
+                    <tbody>
+                    <tr>
+                        <td>Permutaciones:</td>
+                        <td>{combinations}</td>
+                    </tr>
+                    <tr>
+                        <td>P(Encontrar en 1 intento):</td>
+                        <td>{secureProbability}</td>
+                    </tr>
+                    <tr>
+                        <td>Entropía:</td>
+                        <td>{entropy}</td>
+                    </tr>
+                    <tr>
+                        <td>Seguridad:</td>
+                        <td>{security}</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </section>
         </main>
     );
 }
